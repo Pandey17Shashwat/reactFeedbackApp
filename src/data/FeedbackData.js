37ed31a85703c0c1ceefe2f50c05ce15a0e535bf
Pandey17Shashwat <shashwatpandey161@gmail.com>
2024-04-08 18:53:42 +0530

    App has been completed

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const feedbackData = [
+        {
+            id: 1,
+            rating: 10,
+            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis provident deleniti aliquid eos, perferendis reprehenderit mollitia repellat distinctio optio cupiditate, consectetur perspiciatis facere in velit repudiandae officiis amet ad sit',
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis provident deleniti aliquid eos, perferendis reprehenderit mollitia repellat distinctio optio cupiditate, consectetur perspiciatis facere in velit repudiandae officiis amet ad sit',
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis provident deleniti aliquid eos, perferendis reprehenderit mollitia repellat distinctio optio cupiditate, consectetur perspiciatis facere in velit repudiandae officiis amet ad sit',
+        },
+]
+
+export default feedbackData
